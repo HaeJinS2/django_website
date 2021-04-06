8d88db134761cd21f632d2c7409a7e6efefc0d03
Your Name <you@example.com>
2021-04-07 02:58:29 +0900

    blog post list (index)

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=30)
+    title= models.CharField(max_length=30)
     content = models.TextField()
 
     created = models.DateTimeField()
