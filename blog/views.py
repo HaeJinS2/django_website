d6d4f736b3c68746e1a7b840de9271c636300580
Your Name <you@example.com>
2021-04-10 02:18:47 +0900

    1Day

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView
+import os
 
 class PostList(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.order_by('-created')
+        return Post.objects.order_by('-created') #작성일의 역순
 
 # def index(request):
 #     posts = Post.objects.all()
